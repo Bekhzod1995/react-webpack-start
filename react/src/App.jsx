de6c01c5180b34060cab09724821d8cd33be661c
Bekhzod1995 <81156beka@gmail.com>
2020-01-18 23:02:06 +0500

    npm updated

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-    render () {
-        return (
-            <div>
-                <h1 style={{ color: 'green', textAlign: 'center' }}>Hello Beka</h1>
-            </div>
-        );
-    }
+  render () {
+    return (
+      <div>
+        <h1 style={{ color: 'green', textAlign: 'center' }}>Hello Beka</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,38 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+require('@babel/polyfill');
 
 module.exports = {
-    entry: './react/index.js',
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'index_bundle.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: [
-                    'babel-loader',
-                    'eslint-loader'
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            }
+  entry: ['@babel/polyfill', './react/index.js'],
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          'eslint-loader'
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './react/index.html'
-        })
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './react/index.html'
+    })
+  ]
 };
